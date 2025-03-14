f8256d487e68ee30715759e91624ddf754e9a858
tinamanga <christinamanga28@gmail.com>
2025-03-15 01:38:43 +0300

    concatination and interpolation

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+// declare variables and concatinate and  string interpolation
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
+
+// uppercasing the String
+const excitedWelcomeMessage =`${welcomeMessage.toUpperCase()}`+"!";
+// shortening greeting using .slice and extract the first initial of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
